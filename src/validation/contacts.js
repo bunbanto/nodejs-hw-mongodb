f2923229faa3ff010e206aae7f047b7c4b8870da
yurii_fishbakh <bunbanto@gmail.com>
2025-08-12 01:17:16 +0200

    pagination + filters

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -10,7 +10,11 @@ export const createContactSchema = Joi.object({
   phoneNumber: Joi.string().min(3).max(20).required(),
   email: Joi.string().min(3).max(20),
   isFavourite: Joi.boolean(),
-  contactType: Joi.string().min(3).max(20).required(),
+  contactType: Joi.string()
+    .valid('work', 'home', 'personal')
+    .min(3)
+    .max(20)
+    .required(),
 });
 
 export const updateContactSchema = Joi.object({
