09f1595076cd6370a9cbc3726c9457f3a212f3c9
yurii_fishbakh <bunbanto@gmail.com>
2025-08-20 22:56:39 +0200

    add middlewares auth

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -13,8 +13,11 @@ import {
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { isValidId } from '../middlewares/isValidId.js';
 import { validateBody } from '../middlewares/validateBody.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 const router = Router();
+
+router.use(authenticate);
 
 router.get('/', ctrlWrapper(getContactsController));
 
