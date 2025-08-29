de997cb942d3d43d983480fdafd5647e07c896de
yurii_fishbakh <bunbanto@gmail.com>
2025-08-29 20:04:01 +0200

    add resets

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import { loginUserSchema, registerUserSchema } from '../validation/auth.js';
+import {
+  loginUserSchema,
+  registerUserSchema,
+  requestResetEmailSchema,
+  resetPasswordSchema,
+} from '../validation/auth.js';
 import {
   loginUserController,
   logoutUserController,
   refreshUserSessionController,
   registerUserController,
+  requestResetEmailController,
+  resetPasswordController,
 } from '../controllers/auth.js';
 import { validateBody } from '../middlewares/validateBody.js';
 
@@ -28,3 +35,14 @@ router.post(
 router.post('/logout', ctrlWrapper(logoutUserController));
 
 router.post('/refresh', ctrlWrapper(refreshUserSessionController));
+
+router.post(
+  '/request-reset-email',
+  validateBody(requestResetEmailSchema),
+  ctrlWrapper(requestResetEmailController),
+);
+router.post(
+  '/reset-password',
+  validateBody(resetPasswordSchema),
+  ctrlWrapper(resetPasswordController),
+);
--- src/server.js
+++ src/server.js
@@ -35,44 +35,6 @@ export const setupServer = () => {
 
   app.use(errorHandler);
 
-  // app.get('/contacts', async (req, res) => {
-  //   const contacts = await getAllContacts();
-
-  //   res.status(200).json({
-  //     status: 200,
-  //     message: 'Successfully found contacts!',
-  //     data: contacts,
-  //   });
-  // });
-
-  // app.get('/contacts/:contactId', async (req, res, next) => {
-  //   const { contactId } = req.params;
-  //   const contact = await getContactById(contactId);
-
-  //   // Відповідь, якщо контакт не знайдено
-  //   if (!contact) {
-  //     res.status(404).json({
-  //       status: 404,
-  //       message: 'Contact not found',
-  //     });
-  //     return;
-  //   }
-
-  //   // Відповідь, якщо контакт знайдено
-  //   res.status(200).json({
-  //     status: 200,
-  //     message: 'Successfully found contact with id {contactId}!',
-  //     data: contact,
-  //   });
-  // });
-
-  // app.use((req, res, next) => {
-  //   res.status(404).json({
-  //     status: 404,
-  //     message: 'Not found',
-  //   });
-  // });
-
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
