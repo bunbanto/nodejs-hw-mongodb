5abe40798a92168036ce59fac84f3ca314549dbc
yurii_fishbakh <bunbanto@gmail.com>
2025-09-01 22:48:19 +0200

    fixed upload foto and change roure /auth/reset-pwd

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -55,7 +55,18 @@ export const getContactByIdController = async (req, res, next) => {
 
 export const createContactController = async (req, res) => {
   const { _id: userId } = req.user;
-  const contact = await createContact({ ...req.body, userId });
+  const photo = req.file;
+
+  let photoUrl;
+  if (photo) {
+    if (getEnvVar(CLOUDINARY.ENABLE_CLOUDINARY) === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    } else {
+      photoUrl = await saveFileToUploadDir(photo);
+    }
+  }
+
+  const contact = await createContact({ ...req.body, userId, photo: photoUrl });
 
   res.status(201).json({
     status: 201,
--- src/routers/auth.js
+++ src/routers/auth.js
@@ -42,7 +42,7 @@ router.post(
   ctrlWrapper(requestResetEmailController),
 );
 router.post(
-  '/reset-password',
+  '/reset-pwd',
   validateBody(resetPasswordSchema),
   ctrlWrapper(resetPasswordController),
 );
